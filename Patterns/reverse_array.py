91d3d46dd42371b713600b78235b97fcb5fa3edb
JayanthM19 <jayanthmulakalapalli@gmail.com>
2026-06-09 08:07:12 +0530

    Reversing array

---
+++ Patterns/reverse_array.py
@@ -0,0 +1,9 @@
+#https://takeuforward.org/data-structure/reverse-a-given-array
+
+if __name__=="__main__":
+    arr=[5,6,9,8,12,56]
+    print("Original array:", arr)
+    arr.reverse()
+    print("Reversed array:", arr)
+    
+    
