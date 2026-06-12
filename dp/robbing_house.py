0ff1b05faf0ca5964c1266761b37ed368c14d074
JayanthM19 <jayanthmulakalapalli@gmail.com>
2026-06-12 07:01:33 +0530

    Robbing house Q with dp

---
+++ dp/robbing_house.py
@@ -0,0 +1,22 @@
+#https://leetcode.com/problems/house-robber/
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if len(nums)<2:
+            return max(nums)
+        dp=[0]*len(nums)
+        dp[0],dp[1]=nums[0],max(nums[0],nums[1])
+        
+        for i in range(2,len(nums)):
+            
+
+                
+                
+            dp[i]=dp[i-2]+nums[i]
+
+            dp[i]=max(dp[i],dp[i-1])
+        return dp[-1]
