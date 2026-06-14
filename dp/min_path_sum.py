a5c04825dba5f3ec2542f84e87affbd11034d327
JayanthM19 <jayanthmulakalapalli@gmail.com>
2026-06-14 12:09:46 +0530

    Min Path sum using dp

---
+++ dp/min_path_sum.py
@@ -0,0 +1,41 @@
+#https://leetcode.com/problems/minimum-path-sum/description/
+
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m,n=len(grid),len(grid[0])
+
+        # dp=[[0]*n for _ in range(m)]
+
+        # dp[0][0]=grid[0][0]
+
+        # for i in range(1,m):
+        #     dp[i][0]=grid[i][0]+dp[i-1][0]
+        
+        # for i in range(1,n):
+        #     dp[0][i]=grid[0][i]+dp[0][i-1]
+        
+        # for i in range(1,m):
+        #     for j in range(1,n):
+        #         dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1])
+        
+        # return dp[m-1][n-1]
+
+
+        dp=[0]*n
+        dp[0]=grid[0][0]
+
+        for i in range(1,n):
+            dp[i]=dp[i-1]+grid[0][i]
+        
+        for i in range(1,m):
+
+            dp[0]=dp[0]+grid[i][0]
+
+            for j in range(1,n):
+                dp[j]=grid[i][j]+min(dp[j],dp[j-1])
+
+        return dp[n-1]
