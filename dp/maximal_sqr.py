d6fbe887def4a3a0ccbf1214239f8c7201862d15
JayanthM19 <jayanthmulakalapalli@gmail.com>
2026-06-24 20:44:03 +0530

    maximal sqr using dp

---
+++ dp/maximal_sqr.py
@@ -0,0 +1,26 @@
+#https://leetcode.com/problems/maximal-square/description/
+
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        m,n=len(matrix),len(matrix[0])
+
+        dp=[[0]*(n+1) for _ in range(m+1)]
+
+        ans=0
+        
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if matrix[i-1][j-1]=="1":
+                    dp[i][j]=1+min(
+                        dp[i-1][j],
+                        dp[i][j-1],
+                        dp[i-1][j-1]
+                    )
+
+                    ans = max(ans,dp[i][j])
+            
+        return ans*ans
