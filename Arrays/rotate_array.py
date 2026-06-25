f8f89591a68d58e908305074cc80994da6713de6
JayanthM19 <jayanthmulakalapalli@gmail.com>
2026-06-25 20:04:56 +0530

    rotate array q

---
+++ Arrays/rotate_array.py
@@ -0,0 +1,27 @@
+#https://leetcode.com/problems/rotate-array/
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+
+        k%=n
+
+        # for _ in range(k):
+        #     last=nums.pop()
+        #     nums.insert(0,last)
+
+        def reverse(l,r):
+            while l<r:
+
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+                r-=1
+        
+        reverse(0,n-1)
+        reverse(0,k-1)
+        reverse(k,n-1)
