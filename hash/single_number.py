a75091ca71527bad1655ee1bd0bbe5273acc7b9a
JayanthM19 <jayanthmulakalapalli@gmail.com>
2026-06-25 20:28:19 +0530

    single number

---
+++ hash/single_number.py
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/single-number/
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        freq={}
+        for num in nums:
+            freq[num]=freq.get(num,0)+1
+        
+        for key,val in freq.items():
+            if val==1:
+                return key
