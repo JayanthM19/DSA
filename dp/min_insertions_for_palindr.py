9c3d0dfa582c38cecfe805865dff87b5da63c704
JayanthM19 <jayanthmulakalapalli@gmail.com>
2026-06-27 21:21:00 +0530

    min inerstions to make palindrome using dp

---
+++ dp/min_insertions_for_palindr.py
@@ -0,0 +1,24 @@
+#https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
+
+class Solution(object):
+    def minInsertions(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        # return len(s)-len()
+        s2=s[::-1]
+
+        m,n=len(s),len(s2)
+
+        dp=[[0]*(m+1) for _ in range(n+1)]
+
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if s[i-1]==s2[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+
+        return m-dp[m][n]
