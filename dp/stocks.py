a7d1561d7ad91845560a32ae513e971eba3c2c58
JayanthM19 <jayanthmulakalapalli@gmail.com>
2026-07-01 22:18:14 +0530

    stocks q

---
+++ dp/stocks.py
@@ -0,0 +1,19 @@
+#https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        mini=prices[0]
+        profit=0
+
+        for i in range(1,len(prices)):
+
+            cost = prices[i]-mini
+
+            profit = max(profit,cost)
+
+            mini = min(mini,prices[i])
+
+        return profit
