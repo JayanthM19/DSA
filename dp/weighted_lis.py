bceff4a81c41ee4eb31a14a2ecf28be1779d1148
JayanthM19 <jayanthmulakalapalli@gmail.com>
2026-07-07 15:13:16 +0530

    longest increasing subseq q

---
+++ dp/weighted_lis.py
@@ -0,0 +1,23 @@
+#https://leetcode.com/problems/best-team-with-no-conflicts/
+
+class Solution(object):
+    def bestTeamScore(self, scores, ages):
+        """
+        :type scores: List[int]
+        :type ages: List[int]
+        :rtype: int
+        """
+        players = sorted(zip(ages,scores))
+
+        n=len(players)
+
+        dp=[0]*n
+
+        for i in range(n):
+            dp[i]=players[i][1]
+
+            for j in range(i):
+                if players[j][1]<=players[i][1]:
+                    dp[i]=max(dp[i],players[i][1]+dp[j])
+        
+        return max(dp)
